ab0d690b177dd64dacdd49fefead372a247b7d69
Joseph DiNiso <josephdiniso@Josephs-MacBook-Pro.local>
2021-08-26 08:48:24 -0400

    Works, going to try changing the rewards

---
+++ src/agent.py
@@ -0,0 +1,94 @@
+from collections import deque
+import random
+
+import torch
+import numpy as np
+
+from main import Game, TriDirections
+from model import Linear_QNet, QTrainer
+
+MAX_MEM = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+
+class Agent:
+    def __init__(self):
+        self.memory = deque(maxlen=MAX_MEM)
+        self.n_games: int = 0
+        self.epsilon = 0
+        self.gamma = 0.9
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))  # popleft if MAX_MEMORY is reached
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)  # list of tuples
+        else:
+            mini_sample = self.memory
+        for state, action, reward, next_state, done in mini_sample:
+            self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+        return final_move
+
+
+def train():
+    record = 0
+    agent = Agent()
+    game = Game(True)
+    counter: int = 0
+    while True:
+        state_old = game.get_state()
+        move = agent.get_action(state_old)
+        if move == [1, 0, 0]:
+            final_move = TriDirections.STRAIGHT
+        elif move == [0, 1, 0]:
+            final_move = TriDirections.RIGHT
+        else:
+            final_move = TriDirections.LEFT
+        game.set_direction(final_move)
+        reward, done, score = game.game_logic()
+        if reward == 0:
+            counter += 1
+        else:
+            counter = 0
+        state_new = game.get_state()
+        if counter > 100:
+            done = True
+            reward = -10
+        agent.train_short_memory(state_old, move, reward, state_new, done)
+        agent.remember(state_old, move, reward, state_new, done)
+        if done:
+            counter = 0
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+            if score > record:
+                record = score
+                agent.model.save()
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+
+def main():
+    train()
+
+
+if __name__ == "__main__":
+    main()
--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 import random
 from enum import Enum
-import argparse
+import sys
 
+import argparse
 import pygame
-import gym
 import math
 import random
 import numpy as np
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.transforms as T
 
 
 # TODO: Modularize so it can work with any size
@@ -25,12 +19,22 @@ class Directions(Enum):
     LEFT = 4
 
 
+class TriDirections(Enum):
+    STRAIGHT = 0
+    RIGHT = 1
+    LEFT = 2
+
+
 # PyGame Initializations
 block_size: int = 20
-blocks = 35
+blocks = 20
 size = (blocks * block_size + 4, blocks * block_size + 4)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+
+pygame.init()
+pygame.mixer.init(22050, -8, 16, 65536)
+pygame.display.set_caption("PySnake")
 
 # TODO: Figure out a more proper place to store these colors
 BLACK = (0, 0, 0)
@@ -163,39 +167,41 @@ def pause() -> None:
         font = pygame.font.Font('freesansbold.ttf', 15)
 
         # create a text suface object on which text is drawn on it.
-        text = font.render("Paused", True, BLACK, WHITE)
-        text3 = font.render("Continue", True, BLACK, button_color)
+        pause_text = font.render("Paused", True, BLACK, WHITE)
+        continue_text = font.render("Continue", True, BLACK, button_color)
         # create a rectangular object for the text surface object
-        textRect = text.get_rect()
-        textRect3 = text3.get_rect()
+        pause_rect = pause_text.get_rect()
+        continue_rect = continue_text.get_rect()
 
         # set the center of the rectangular object.
-        textRect.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 - 65)
-        textRect3.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 + 75)
-        screen.blit(text, textRect)
-        screen.blit(text3, textRect3)
+        pause_rect.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 - 65)
+        continue_rect.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 + 75)
+        screen.blit(pause_text, pause_rect)
+        screen.blit(continue_text, continue_rect)
         pygame.display.flip()
         pygame.display.update()
 
 
 class Game:
-    def __init__(self) -> None:
+    def __init__(self, training: bool) -> None:
         """
         Generates a blank game and initializes
 
         Returns:
             None
         """
+        self.training: bool = training
+        self.iterations: int = 0
         self.snakes = []
         self.score: int = 0
-        self.fruit = Fruit(self.snakes)
+        self.fruit: Fruit = Fruit(self.snakes)
         self.snakes = []
-        self.snake = Snake(12 * 20 + 10, 12 * 20 + 10, head=True)
-        self.snake.x = 12 * 20 + 10
-        self.snake.y = 12 * 20 + 10
+        self.snake = Snake(blocks // 2 * 20 + 10, blocks // 2 * 20 + 10, head=True)
         self.snake.dir = Directions.UP
         self.snake.child = None
         self.snakes.append(self.snake)
+        self.prev_time_first: int = 0
+        self.game_time = 1 if training else 60
 
     def move_snake(self, change: bool) -> None:
         """
@@ -209,7 +215,7 @@ class Game:
 
     def check_collisions(self) -> int:
         """
-        Checks if snake has collided with borders, flower, or itself. Will return the score for calculating the loss
+        Checks if snake has collided with borders, flower, or itself. Returns the score for calculating the loss
 
         Returns:
             (int) Score
@@ -221,6 +227,7 @@ class Game:
                     self.fruit = Fruit(self.snakes)
                     child = snake.create()
                     self.snakes.append(child)
+                    self.score += 1
                     return 10
                 if snake.x >= size[0] or snake.y >= size[0] or snake.x <= 0 or snake.y <= 0:
                     self.die()
@@ -238,6 +245,14 @@ class Game:
         Returns:
             None
         """
+        screen.fill(WHITE)
+        font = pygame.font.Font('freesansbold.ttf', 15)
+        pygame.draw.rect(screen, GREEN, [0, 0, size[0], size[1]])
+        pygame.draw.rect(screen, WHITE, [2, 2, size[0] - 4, size[1] - 4])
+        score_text = font.render(f"Score:{str(self.score)}", False, (0, 0, 0))
+        screen.blit(score_text, (15, 15))
+        iter_text = font.render(f"Iterations:{str(self.iterations)}", False, (0, 0, 0))
+        screen.blit(iter_text, (size[0] - 100, 15))
         pygame.draw.rect(screen, LIGHT_RED, [self.fruit.x - 10, self.fruit.y - 10, 20, 20])
         pygame.draw.rect(screen, RED, [self.fruit.x - 8, self.fruit.y - 8, 16, 16])
         for snake in self.snakes:
@@ -255,20 +270,33 @@ class Game:
         self.score: int = 0
         self.fruit = Fruit(self.snakes)
         self.snakes = []
-        self.snake = Snake(12 * 20 + 10, 12 * 20 + 10, head=True)
-        self.snake.x = 12 * 20 + 10
-        self.snake.y = 12 * 20 + 10
+        self.snake = Snake(blocks // 2 * 20 + 10, blocks // 2 * 20 + 10, head=True)
         self.snake.dir = Directions.UP
         self.snake.child = None
         self.snakes.append(self.snake)
 
+    def inc_games(self) -> None:
+        self.iterations += 1
+
+    def score(self) -> int:
+        return self.score
+
+    def set_game_time(self, time: int):
+        self.game_time = time
+
     def die(self) -> None:
         """
-        Occurs if a collision with itself or the wall occurs
+        Called if a collision with itself or the wall occurs
+
+        Waits for a user input to play again if not training, otherwise automatically starts next game.
 
         Returns:
             None
         """
+        self.inc_games()
+        if self.training:
+            self.reset()
+            return
         dead = True
         while dead:
             pos = pygame.mouse.get_pos()
@@ -289,40 +317,99 @@ class Game:
             pygame.draw.rect(screen, BLACK, [(size[0] - 4) // 2 - 100, (size[1] - 4) // 2 - 100, 200, 200])
             pygame.draw.rect(screen, WHITE, [size[0] // 2 - 100, size[1] // 2 - 100, 196, 196])
             pygame.draw.rect(screen, button_color, [(size[0] - 4) // 2 - 50, size[1] // 2 + 50, 100, 40])
-
             font = pygame.font.Font('freesansbold.ttf', 15)
-            text = font.render("Dead", True, BLACK, WHITE)
-            text3 = font.render("Play Again", True, BLACK, button_color)
-            textRect = text.get_rect()
-            textRect3 = text3.get_rect()
-            textRect.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 - 65)
-            textRect3.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 + 75)
-            screen.blit(text, textRect)
-            screen.blit(text3, textRect3)
+            dead_text = font.render("Dead", True, BLACK, WHITE)
+            play_text = font.render("Play Again", True, BLACK, button_color)
+            dead_rect = dead_text.get_rect()
+            play_rect = play_text.get_rect()
+            dead_rect.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 - 65)
+            play_rect.center = ((size[0] - 4) // 2, (size[1] - 4) // 2 + 75)
+            screen.blit(dead_text, dead_rect)
+            screen.blit(play_text, play_rect)
             pygame.display.flip()
             pygame.display.update()
         self.reset()
 
-    def game_loop(self) -> None:
+    def get_state(self) -> np.array:
         """
-        Game loop to take player input and process it
+        Returns state of the game as a numpy array
+
+        Returns:
+            (np.array): Array of the following form:
+            [danger-straight, danger-right, danger-left,
+            direction-left, direction-right, direction-up, direction-down,
+            food-left, food-right, food-up, food-down]
+        """
+        grid = np.zeros((blocks, blocks))
+        head_coord = ((self.snake.x - 10) // 20, (self.snake.y - 10) // 20)
+        for snake in self.snakes:
+            x_index = (snake.x - 10) // 20
+            y_index = (snake.y - 10) // 20
+            if blocks > x_index >= 0 and blocks > y_index >= 0:
+                grid[x_index, y_index] = 1
+        fields = np.zeros(11, dtype=bool)
+        if head_coord[0] < 20 and head_coord[1] < 20:
+            if self.snake.dir == Directions.UP:
+                fields[5] = True
+                if head_coord[1] <= 0 or grid[head_coord[0], head_coord[1] - 1] == 1:
+                    fields[0] = True
+                if head_coord[0] >= blocks - 1 or grid[head_coord[0] + 1, head_coord[1]] == 1:
+                    fields[1] = True
+                if head_coord[0] <= 0 or grid[head_coord[0] - 1, head_coord[1]] == 1:
+                    fields[2] = True
+            if self.snake.dir == Directions.DOWN:
+                fields[6] = True
+                if head_coord[1] >= blocks - 1 or grid[head_coord[0], head_coord[1] + 1] == 1:
+                    fields[0] = True
+                if head_coord[0] <= 0 or grid[head_coord[0] - 1, head_coord[1]] == 1:
+                    fields[1] = True
+                if head_coord[0] >= blocks - 1 or grid[head_coord[0] + 1, head_coord[1]] == 1:
+                    fields[2] = True
+            if self.snake.dir == Directions.LEFT:
+                fields[3] = True
+                if head_coord[0] <= 0 or grid[head_coord[0] - 1, head_coord[1]] == 1:
+                    fields[0] = True
+                if head_coord[1] <= 0 or grid[head_coord[0], head_coord[1] - 1] == 1:
+                    fields[1] = True
+                if head_coord[1] >= blocks - 1 or grid[head_coord[0], head_coord[1] + 1] == 1:
+                    fields[2] = True
+            if self.snake.dir == Directions.RIGHT:
+                fields[4] = True
+                if head_coord[0] >= blocks - 1 or grid[head_coord[0] - 1, head_coord[1]] == 1:
+                    fields[0] = True
+                if head_coord[1] >= blocks - 1 or grid[head_coord[0], head_coord[1] + 1] == 1:
+                    fields[1] = True
+                if head_coord[1] >= 0 or grid[head_coord[0], head_coord[1] - 1] == 1:
+                    fields[2] = True
+        fruit_coords = ((self.fruit.x - 10) // 20, (self.fruit.y - 10) // 20)
+        if fruit_coords[0] < head_coord[0]:
+            fields[7] = True
+        if fruit_coords[0] > head_coord[0]:
+            fields[8] = True
+        if fruit_coords[1] < head_coord[1]:
+            fields[9] = True
+        if fruit_coords[1] > head_coord[1]:
+            fields[10] = True
+        return fields
+
+    def key_input(self) -> None:
+        """
+        Either takes user input for movement or queries the NN for the next move
 
         Returns:
             None
         """
-        done = False
-        prev_time_first = 0
-        prev_time_second = 0
-        prev_dir = Directions.UP
-
-        while not done:
-            if pygame.time.get_ticks() - prev_time_first > 120:
-                prev_dir = self.snake.dir
-                prev_time_first = pygame.time.get_ticks()
+        if self.training:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    done = True
-                elif event.type == pygame.KEYDOWN:
+                    pygame.quit()
+                    sys.exit()
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a and self.snake.dir != Directions.RIGHT:
                         self.snake.instruction = Directions.LEFT
                     elif event.key == pygame.K_d and self.snake.dir != Directions.LEFT:
@@ -333,30 +420,59 @@ class Game:
                         self.snake.instruction = Directions.DOWN
                     elif event.key == pygame.K_p:
                         pause()
-            screen.fill(WHITE)
-            font = pygame.font.Font('freesansbold.ttf', 15)
-            pygame.draw.rect(screen, GREEN, [0, 0, size[0], size[1]])
-            pygame.draw.rect(screen, WHITE, [2, 2, size[0] - 4, size[1] - 4])
-            score_text = font.render(str(self.score), False, (0, 0, 0))
-            screen.blit(score_text, (15, 15))
-            self.animate()
-            clock.tick(30)
-            if pygame.time.get_ticks() - prev_time_second > 120:
-                if prev_dir != self.snake.dir:
-                    change = True
-                else:
-                    change = False
-                score: int = self.check_collisions()
-                self.move_snake(change)
-                prev_time_second = pygame.time.get_ticks()
-            pygame.display.flip()
+
+    def set_direction(self, direction):
+        if direction == TriDirections.LEFT:
+            if self.snake.dir == Directions.UP:
+                self.snake.instruction = Directions.LEFT
+            elif self.snake.dir == Directions.RIGHT:
+                self.snake.instruction = Directions.UP
+            elif self.snake.dir == Directions.LEFT:
+                self.snake.instruction = Directions.DOWN
+            elif self.snake.dir == Directions.DOWN:
+                self.snake.instruction = Directions.RIGHT
+        elif direction == TriDirections.RIGHT:
+            if self.snake.dir == Directions.UP:
+                self.snake.instruction = Directions.RIGHT
+            elif self.snake.dir == Directions.RIGHT:
+                self.snake.instruction = Directions.DOWN
+            elif self.snake.dir == Directions.LEFT:
+                self.snake.instruction = Directions.UP
+            elif self.snake.dir == Directions.DOWN:
+                self.snake.instruction = Directions.LEFT
+
+    def game_logic(self):
+        while pygame.time.get_ticks() - self.prev_time_first <= self.game_time:
+            pass
+        prev_dir = self.snake.dir
+        self.prev_time_first = pygame.time.get_ticks()
+        if prev_dir != self.snake.dir:
+            change = True
+        else:
+            change = False
+        reward: int = self.check_collisions()
+        self.get_state()
+        self.move_snake(change)
+        self.key_input()
+        self.animate()
+        clock.tick(60)
+        pygame.display.flip()
+        done = True if reward == -10 else False
+        return reward, done, self.score
+
+    def game_loop(self) -> None:
+        """
+        Game loop to take player input and process it
+
+        Returns:
+            None
+        """
+        while True:
+            self.game_logic()
 
 
 def main():
-    pygame.init()
-    pygame.mixer.init(22050, -8, 16, 65536)
-    pygame.display.set_caption("PySnake")
-    game = Game()
+    game = Game(False)
     game.game_loop()
 
 
